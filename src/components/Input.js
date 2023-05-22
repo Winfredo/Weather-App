2631de841ead8fd624698b8dce98f218d98f6e3e
Winfredo <winfrednukpe2002@gmail.com>
2023-05-22 13:47:59 +0000

    Initial commit

---
+++ src/components/Input.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './input.css'
+import { BsSearch } from 'react-icons/fa';
+
+const Input = () => {
+  return (
+    <form className='input'>
+    <input type={'text'} placeholder='Please enter your location.'>
+    </input>
+    <span>
+    <BsSearch/>
+
+    </span>
+    </form>
+  )
+}
+
+export default Input
